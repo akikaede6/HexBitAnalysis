4a2ae4d8952824925a7d3d4f9bc14a3968fbfd0d
rrcao <rrcao@ambarella.com>
2015-01-27 22:42:40 +0800

    TBC

--- hexbitanalysis.cpp
+++ hexbitanalysis.cpp
@@ -1,3 +1,5 @@
+#include <QDebug>
+#include <QtGui>
 #include "hexbitanalysis.h"
 #include "ui_hexbitanalysis.h"
 
@@ -6,18 +8,117 @@ HexBitAnalysis::HexBitAnalysis(QWidget *parent) :
     ui(new Ui::HexBitAnalysis)
 {
     int i;
+
     ui->setupUi(this);
 
-//    for (i = ui->LabelOrigLayout->layout()->count() - 1; i >= 0; i--) {
-//        QLayoutItem *it = ui->LabelOrigLayout->layout()->itemAt(i);
-//        QLabel *orig = qobject_cast<QLabel *>(it->widget());
-//        orig->setStyleSheet("background-color:rgbrgb(85, 170, 255)");
-//    }
+    LabelOrig0.clear();
+    LabelOrig0.append(ui->label_orig0_0);
+    LabelOrig0.append(ui->label_orig0_1);
+    LabelOrig0.append(ui->label_orig0_2);
+    LabelOrig0.append(ui->label_orig0_3);
+    LabelOrig0.append(ui->label_orig0_4);
+    LabelOrig0.append(ui->label_orig0_5);
+    LabelOrig0.append(ui->label_orig0_6);
+    LabelOrig0.append(ui->label_orig0_7);
+    LabelOrig0.append(ui->label_orig0_8);
+    LabelOrig0.append(ui->label_orig0_9);
+    LabelOrig0.append(ui->label_orig0_10);
+    LabelOrig0.append(ui->label_orig0_11);
+    LabelOrig0.append(ui->label_orig0_12);
+    LabelOrig0.append(ui->label_orig0_13);
+    LabelOrig0.append(ui->label_orig0_14);
+    LabelOrig0.append(ui->label_orig0_15);
+    LabelOrig0.append(ui->label_orig0_16);
+    LabelOrig0.append(ui->label_orig0_17);
+    LabelOrig0.append(ui->label_orig0_18);
+    LabelOrig0.append(ui->label_orig0_19);
+    LabelOrig0.append(ui->label_orig0_20);
+    LabelOrig0.append(ui->label_orig0_21);
+    LabelOrig0.append(ui->label_orig0_22);
+    LabelOrig0.append(ui->label_orig0_23);
+    LabelOrig0.append(ui->label_orig0_24);
+    LabelOrig0.append(ui->label_orig0_25);
+    LabelOrig0.append(ui->label_orig0_26);
+    LabelOrig0.append(ui->label_orig0_27);
+    LabelOrig0.append(ui->label_orig0_28);
+    LabelOrig0.append(ui->label_orig0_29);
+    LabelOrig0.append(ui->label_orig0_30);
+    LabelOrig0.append(ui->label_orig0_31);
 
-   // ui->pushButton_5->setDown(true);
+    GrpboxOrig0.clear();
+    GrpboxOrig0.append(ui->groupBox_0_0);
+    GrpboxOrig0.append(ui->groupBox_0_1);
+    GrpboxOrig0.append(ui->groupBox_0_2);
+    GrpboxOrig0.append(ui->groupBox_0_3);
+    GrpboxOrig0.append(ui->groupBox_0_4);
+    GrpboxOrig0.append(ui->groupBox_0_5);
+    GrpboxOrig0.append(ui->groupBox_0_6);
+    GrpboxOrig0.append(ui->groupBox_0_7);
+    GrpboxOrig0.append(ui->groupBox_0_8);
+    GrpboxOrig0.append(ui->groupBox_0_9);
+    GrpboxOrig0.append(ui->groupBox_0_10);
+    GrpboxOrig0.append(ui->groupBox_0_11);
+    GrpboxOrig0.append(ui->groupBox_0_12);
+    GrpboxOrig0.append(ui->groupBox_0_13);
+    GrpboxOrig0.append(ui->groupBox_0_14);
+    GrpboxOrig0.append(ui->groupBox_0_15);
+    GrpboxOrig0.append(ui->groupBox_0_16);
+    GrpboxOrig0.append(ui->groupBox_0_17);
+    GrpboxOrig0.append(ui->groupBox_0_18);
+    GrpboxOrig0.append(ui->groupBox_0_19);
+    GrpboxOrig0.append(ui->groupBox_0_20);
+    GrpboxOrig0.append(ui->groupBox_0_21);
+    GrpboxOrig0.append(ui->groupBox_0_22);
+    GrpboxOrig0.append(ui->groupBox_0_23);
+    GrpboxOrig0.append(ui->groupBox_0_24);
+    GrpboxOrig0.append(ui->groupBox_0_25);
+    GrpboxOrig0.append(ui->groupBox_0_26);
+    GrpboxOrig0.append(ui->groupBox_0_27);
+    GrpboxOrig0.append(ui->groupBox_0_28);
+    GrpboxOrig0.append(ui->groupBox_0_29);
+    GrpboxOrig0.append(ui->groupBox_0_30);
+    GrpboxOrig0.append(ui->groupBox_0_31);
 }
 
 HexBitAnalysis::~HexBitAnalysis()
 {
     delete ui;
+}
+
+void HexBitAnalysis::on_lineEdit_Input_textChanged(const QString &arg1)
+{
+    QString input;
+    int i;
+    ulong hex;
+    bool ok;
+
+    input = ui->lineEdit_Input->text().trimmed();
+    if (input.startsWith("0x"))
+        ui->lineEdit_Input->setText(input.remove(0, 2));
+
+    hex = input.toULong(&ok, 16);
+
+    for (i = 0; i < 32; i++) {
+        if ((1 << i) & hex) {
+            LabelOrig0[i]->setText("1");
+            LabelOrig0[i]->setStyleSheet(QString::fromUtf8("QLabel{\n"
+                                            "	background-color:rgb(255, 0, 0);\n"
+                                            "}"));
+            GrpboxOrig0[i]->setStyleSheet(QString::fromUtf8("QGroupBox{\n"
+                                                            "	border:4px inset lightGray;\n"
+                                                            "	border-radius: 3px;\n"
+                                                            "	background: white\n"
+                                                            "}"));
+        } else {
+            LabelOrig0[i]->setText("0");
+            LabelOrig0[i]->setStyleSheet(QString::fromUtf8("QLabel{\n"
+                                            "	background-color:rgb(85, 170, 255);\n"
+                                            "}"));
+            GrpboxOrig0[i]->setStyleSheet(QString::fromUtf8("QGroupBox{\n"
+                                                            "	border:4px outset lightGray;\n"
+                                                            "	border-radius: 3px;\n"
+                                                            "	background: white\n"
+                                                            "}"));
+        }
+    }
 }
--- hexbitanalysis.h
+++ hexbitanalysis.h
@@ -2,6 +2,7 @@
 #define HEXBITANALYSIS_H
 
 #include <QDialog>
+#include <QtGui>
 
 namespace Ui {
 class HexBitAnalysis;
@@ -15,8 +16,14 @@ public:
     explicit HexBitAnalysis(QWidget *parent = 0);
     ~HexBitAnalysis();
     
+private slots:
+    void on_lineEdit_Input_textChanged(const QString &arg1);
+
 private:
     Ui::HexBitAnalysis *ui;
+
+    QList<QLabel *> LabelOrig0;
+    QList<QGroupBox *> GrpboxOrig0;
 };
 
 #endif // HEXBITANALYSIS_H
